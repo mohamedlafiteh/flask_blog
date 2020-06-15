5e5a20c1ae358dd86e0a9b1ce75aaf4bf97eaea0
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-06-15 15:28:55 +0100

    Firt commit

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {'author': 'Ali lafiteh',
+     'title': 'blog post 1',
+     'content': 'first post content',
+     'date_posted': 'April 20, 2018'
+     },
+    {'author': 'lauren curr',
+     'title': 'blog post 2',
+     'content': 'first post content',
+     'date_posted': 'April 21, 2018'
+     }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
