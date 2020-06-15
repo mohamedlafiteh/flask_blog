22f60e085ddcdf860c4634e6bfaf50764fc66795
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-06-15 20:15:33 +0100

    Add forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '07cdc7c3afc425f6e57c27b327d83ed1'
 
 posts = [
     {'author': 'Ali lafiteh',
@@ -24,6 +26,28 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('you have been logged in', 'success')
+            return redirect(url_for('home'))
+    else:
+        flash('Login unsuccessful', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
